c90900bc6a744f5a8ab774e546de35248f215694
Gbenga <oofhc@mst.edu>
2016-03-27 21:07:55 -0500

    Added Hardware Watchdog

--- DriveBoard-Software.ino
+++ DriveBoard-Software.ino
@@ -1,6 +1,10 @@
 // Standard C integers of specific size (such as int16_t)
 #include <stdint.h>
 
+// Include Tiva Watchdog Driver Library
+#include <driverlib/sysctl.h>
+#include <driverlib/watchdog.h>
+
 // Energia libraries used by RoveWare itself
 #include <SPI.h>
 #include <Ethernet.h>
@@ -45,13 +49,31 @@ size_t data_size = 0;
 const int16_t RED_MIN_SPEED     =  -1000;
 const int16_t RED_MAX_SPEED     =   1000;
 
+const int position_zero = map(0, RED_MIN_SPEED, RED_MAX_SPEED, 0,  180);
+
 int16_t motors_speed = 0;
+
+uint32_t ROVECOMMWATCHDOG = WATCHDOG1_BASE;
+#define WATCHDOG_LENGTH 30000000 //Randomly chosen number, takes roughly 1.5 seconds
+
+void stopDriving() { 
+  //Serial.println("Watchdog Triggered");
+  
+  left_front_motor.write(position_zero);
+  left_middle_motor.write(position_zero);
+  left_back_motor.write(position_zero);
+  right_front_motor.write(position_zero);
+  right_middle_motor.write(position_zero);  
+  right_back_motor.write(position_zero);
+  
+  WatchdogIntClear(ROVECOMMWATCHDOG);
+}//end function
 
 void setup()
 {
   //Serial.begin(9600);  
   roveComm_Begin(192, 168, 1, 130); 
-   
+  
   //Tiva Pin: PG0 = 37
   left_front_motor.attach(37, RC_PWM_MIN_SPEED, RC_PWM_MAX_SPEED);
   //Tiva Pin: PF3 = 38
@@ -65,14 +87,24 @@ void setup()
   right_middle_motor.attach(79, RC_PWM_MIN_SPEED, RC_PWM_MAX_SPEED);
   //PK_5
   right_back_motor.attach(78, RC_PWM_MIN_SPEED, RC_PWM_MAX_SPEED);
-   
+  
+  // Enable Watchdog Timer 1; supposedly timer 0 has a conflict in Energia, unconfirmed
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_WDOG1);
+  
+  WatchdogUnlock(ROVECOMMWATCHDOG);
+  WatchdogReloadSet(ROVECOMMWATCHDOG, WATCHDOG_LENGTH);
+  WatchdogIntRegister(ROVECOMMWATCHDOG, stopDriving);
+  WatchdogIntEnable(ROVECOMMWATCHDOG);
+  WatchdogResetDisable(ROVECOMMWATCHDOG);
+  WatchdogEnable(ROVECOMMWATCHDOG);
+  
 }//end setup
 
 void loop()
 {
   //If there is no message data_id gets set to zero
   roveComm_GetMsg(&data_id, &data_size, &motors_speed);
-  
+
   switch (data_id) 
   {   
     //Don't do anything for data_id zero 
@@ -88,7 +120,7 @@ void loop()
       left_middle_motor.write(position);
       left_back_motor.write(position); 
       //Serial.println(motors_speed);
-      //Serial.println(pos);       
+      //Serial.println(pos);
       break;
     
     case DRIVE_RIGHT_MOTORS_BY_IP: 
@@ -108,5 +140,9 @@ void loop()
       break;
   
   }//endswitch
+  
+  if (data_id != 0) {
+    WatchdogIntClear(ROVECOMMWATCHDOG);
+  }//end if
 
 }//end loop
