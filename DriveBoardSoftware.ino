c33f00348376e6d46c54eba9d605534d87bbe50b
Abe Morris <am452@mst.edu>
2017-01-21 14:39:57 -0600

    Made changes to Watchdog and RoveComm code
    Tested original code, which didn't work perfectly. Made many changes to
    make that work. Code should be operational now.

--- DriveBoardSoftware.ino
+++ DriveBoardSoftware.ino
@@ -1,4 +1,4 @@
-// include standard C integers
+// Standard C integers of specific size (such as int16_t)
 #include <stdint.h>
 
 // Include Tiva Watchdog Driver Library
@@ -6,47 +6,75 @@
 #include <driverlib/watchdog.h>
 #include <driverlib/rom_map.h>
 
-// Libraries used by RoveWare
+#include <stdio.h>
+#include <stdlib.h>
+
+// Energia libraries used by RoveWare itself
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-// Roveware libraries
+// RoveWare 
 #include "RoveEthernet.h"
 #include "RoveComm.h"
 
-const byte HOLD_TIME = 50; // time to hold the speed in ms. (for test routine only)
-// constants for RoveComm
-const uint16_t SYSTEM_ABORT  = 199;
-const uint16_t LEFT_SIDE_IP  = 100;
-const uint16_t RIGHT_SIDE_IP = 101;
-const uint32_t ROVECOMM_TIMEOUT = 16400000; // ~1.5 seconds
-const uint32_t ROVECOMM_WATCHDOG = WATCHDOG1_BASE;
-
-// speed constants
-const byte MAX_FORWARD = 255;
-const byte ZERO_SPEED = 127;
-const byte MAX_REVERSE = 0;
-
-// speed commands sent from RED XBox Controller values
-const short RED_MAX = 1000;
-const short RED_MIN = -1000;
-
-byte speed = 100; // speed variable for test routine
-byte speeds[6];   // array of speeds for each motor
-
+// RED sends us a data_id 100 or 101 with data_size of two bytes and data is a two byte motor_speed
 uint16_t data_id = 0;
 size_t data_size = 0; 
 bool system_abort_flag = false;
 
-short last_time = 0;
+// RoveComm Codes
+const uint16_t DRIVE_LEFT_MOTORS_BY_IP = 100;
+const uint16_t DRIVE_RIGHT_MOTORS_BY_IP = 101;
+const uint16_t DRIVE_DEVICE_SYSTEM_ABORT = 199;
+
+// Speed consts
+const byte MAX_FORWARD = 255;
+const byte ZERO_SPEED  = 127;
+const byte MAX_REVERSE = 0;
+
+// RED sends xbox controller input to us
+const int16_t RED_MAX_SPEED     =   1000;
+const int16_t RED_MIN_SPEED     =  -1000;
+
+int16_t speed = 0;
+byte right_speed = 0;
+byte left_speed = 0;
+
+const uint32_t ROVECOMM_WATCHDOG_TIMEOUT_TICKS = 16400000; // ~1.5 seconds
+const uint32_t ROVECOMM_WATCHDOG = WATCHDOG1_BASE;
+
+void roveWatchdogClear()
+{
+  WatchdogIntClear(ROVECOMM_WATCHDOG);
+}
+
+void RoveCommWatchdog_begin(void (*roveWatchdogIntHandler)(void), const uint32_t time_out_millis)
+{   
+  // Enable Watchdog Timer 1; supposedly timer 0 has a conflict in Energia, unconfirmed
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_WDOG1);  
+  WatchdogUnlock(ROVECOMM_WATCHDOG);
+  WatchdogReloadSet(ROVECOMM_WATCHDOG, ROVECOMM_WATCHDOG_TIMEOUT_TICKS);
+  
+  WatchdogIntRegister(ROVECOMM_WATCHDOG, roveWatchdogIntHandler);
+  
+  WatchdogIntEnable(ROVECOMM_WATCHDOG);
+  WatchdogResetDisable(ROVECOMM_WATCHDOG);
+  WatchdogEnable(ROVECOMM_WATCHDOG);
+}
 
 void setup()
 {
-  // begin RoveComm with assigned IP
-  roveComm_Begin(192,168,1,130);
-   
-  // open motor controller serial channels
+  Serial.begin(9600);  
+  roveComm_Begin(192, 168, 1, 130); 
+  // Open RS232 Channels to the motor controllers
+  // Serial Assignments:
+  //  Serial2: Rear Left
+  //  Serial3: Front Left
+  //  Serial4: Middle Left
+  //  Serial5: Middle Right
+  //  Serial6: Front Right
+  //  Serial7: Rear Right
   Serial2.begin(19200);
   Serial3.begin(19200);
   Serial4.begin(19200);
@@ -54,25 +82,21 @@ void setup()
   Serial6.begin(19200);
   Serial7.begin(19200);
 
-  // test routine for motor controller communication (remove for real operation)
-  test();
-  
-  // set speeds to zero to begin the loop
-  speed = ZERO_SPEED;
-  for(byte i = 0; i < 6; i++)
-  {
-    speeds[i] = ZERO_SPEED;
-  }
-  RoveCommWatchdog_begin(Estop); //! unsure about second parameter (see last years code)
+  right_speed = ZERO_SPEED;
+  left_speed = ZERO_SPEED;
+  RoveCommWatchdog_begin(roveEstopDriveMotors, 1500); // not sure what second arg is... used to be RC_PWM_ZERO_SPEED
 }
-void loop() 
-{  
+
+void loop()
+{
   //Provide System Abort
   if(!system_abort_flag)
   { 
     //If there is no message data_id gets set to zero
     roveComm_GetMsg(&data_id, &data_size, &speed);
-    
+
+    speed += 1000; // change possible range to [0, 1999]
+    speed = (speed*MAX_FORWARD)/1999; // adjust the value for expected speed range
     switch (data_id) 
     {   
       //Don't do anything for data_id zero 
@@ -80,133 +104,48 @@ void loop()
         break;
         
       case DRIVE_LEFT_MOTORS_BY_IP: 
-        speeds[0] = speeds[1] = speeds[2] = speed; // change speed values for left side
+        left_speed = speed;
+        Serial.println(left_speed);
         break;
       
-      case DRIVE_RIGHT_MOTORS_BY_IP:       
-        speeds[3] = speeds[4] = speeds[5] = speed; // change speed values for right side
+      case DRIVE_RIGHT_MOTORS_BY_IP: 
+        right_speed = speed;
+        Serial.println(right_speed);
         break;
       
       default:
         break;  
     }
-    if (data_id != 0) 
+    
+    if (data_id != 0)
     {
       roveWatchdogClear();
     }
-    
-  }else{   
+  }
+  else
+  {   
     roveComm_SendMsg(DRIVE_DEVICE_SYSTEM_ABORT, data_size, &system_abort_flag);
     delay(1000); 
   }
-  
   write_speeds();
 }
 
-void Estop()
-{
-  for(byte i = 0; i < 6; i++)
-  {
-    speeds[i] = 0;
-  }
-  write_speeds();
-  system_abort_flag = true; //! sets the flag to true if the watchdog gets tripped
+void roveEstopDriveMotors() 
+{ 
+  right_speed = ZERO_SPEED;
+  left_speed  = ZERO_SPEED;
+  Serial.println("Watchdog Triggered");
+  roveWatchdogClear();
 }
+
 void write_speeds()
 {
-  // need to determine the channels correlations to the motors
-  Serial2.write(speeds[0]);
-  Serial3.write(speeds[0]);
-  Serial4.write(speeds[0]);
-  Serial5.write(speeds[0]);
-  Serial6.write(speeds[0]);
-  Serial7.write(speeds[0]);
-  delay(1); //! determine if a delay is necessary after writing the speeds
+  Serial2.write(left_speed);
+  Serial3.write(left_speed);
+  Serial4.write(left_speed);
+  Serial5.write(right_speed);
+  Serial6.write(right_speed);
+  Serial7.write(right_speed);
+  delay(1); //! determine if delay is necessary
 }
-void forward_cycle()
-{
-  short accel = 1;
-  speed = ZERO_SPEED+1; 
-  last_time = 0;
-  
-  while(speed >= ZERO_SPEED)
-  {
-    if(millis() - last_time >= HOLD_TIME) // keep sending speeds for HOLD_TIME milliseconds
-    {
-      speed += accel; // add acceleration to the speed.
-
-      if(speed >= MAX_FORWARD) // if speed hits max...
-        accel *= -1; // negate acceleration.
-        
-      last_time = millis(); // update time
-      Serial.print(speed + " "); // output the current speed      
-    }
-    // write speeds
-    Serial2.write(speed);
-    Serial3.write(speed);
-    Serial4.write(speed);
-    Serial5.write(speed);
-    Serial6.write(speed);
-    Serial7.write(speed);
-  }
-  Serial.println();
-}
-
-void reverse_cycle()
-{
-  short accel = 1;
-  speed = ZERO_SPEED - 1;
-  last_time = 0;
-  
-  while(speed <= ZERO_SPEED)
-  {
-    if(millis() - last_time >= HOLD_TIME) // keep sending speed fot HOLD_TIME milliseconds
-    {
-      speed -= accel; // add acceleration to speed (negative accel increases speed since it is increasing in reverse direction.
-
-      if(speed <= MAX_REVERSE) // if speed hits max...
-        accel *= -1; // negate acceleration.
-
-      last_time = millis(); // update time
-      Serial.print(speed + " "); // output the current speed          
-    }
-    // write speeds
-    Serial2.write(speed);
-    Serial3.write(speed);
-    Serial4.write(speed);
-    Serial5.write(speed);
-    Serial6.write(speed);
-    Serial7.write(speed);
-  }
-  Serial.println();
-}
-void test()
-{
-  delay(2000); // wait for a bit before starting test routine
-  forward_cycle();
-  reverse_cycle();
-}
-/* -------------------------------- Begin Watchdog Code -------------------------------- */
-void roveWatchdogClear()
-{
-  WatchdogIntClear(ROVECOMM_WATCHDOG);
-}
-void RoveCommWatchdog_begin(void (*roveWatchdogIntHandler)(void))
-{   
-  // Enable Watchdog Timer 1; supposedly timer 0 has a conflict in Energia, unconfirmed
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_WDOG1);  
-  WatchdogUnlock(ROVECOMM_WATCHDOG);
-  WatchdogReloadSet(ROVECOMM_WATCHDOG, ROVECOMM_TIMEOUT);
-  
-  WatchdogIntRegister(ROVECOMM_WATCHDOG, roveWatchdogIntHandler);
-  
-  WatchdogIntEnable(ROVECOMM_WATCHDOG);
-
-  WatchdogResetDisable(ROVECOMM_WATCHDOG);
-  WatchdogEnable(ROVECOMM_WATCHDOG);
-}
-/* --------------------------------- End Watchdog Code --------------------------------- */
-
-
-
 
