1b26ce68670e8e247583206c2849b30255ced8b1
andrewvh4 <31864022+andrewvh4@users.noreply.github.com>
2018-05-30 15:19:14 -0500

    Hardware Testing

--- DriveBoardSoftware/DriveBoardSoftware.ino
+++ DriveBoardSoftware/DriveBoardSoftware.ino
@@ -29,10 +29,13 @@ byte right_drive_speed       = DRIVE_ZERO;
 const byte LED_COUNT         = 60;
 const byte LED_SPI_MODULE    = 3;
 
+bool notification_on                 = 0;
+const int HEADLIGHT_ON_OFF_FOR_LOOPS = 1000;
+int headlight_loop_count             = 0;
+
 Servo DropBay1;
 Servo DropBay2;
 
-RoveCommEthernetUdp RoveComm;
 RoveWatchdog        Watchdog;
 Adafruit_NeoPixel   NeoPixel(LED_COUNT, LED_SPI_MODULE);
 
@@ -42,7 +45,7 @@ void roveEstopDriveMotors();
 
 void setup()
 {
-  //Serial.begin( 9600);  // todo => debug only 
+  Serial.begin( 9600); 
   Serial2.begin(19200); 
   Serial3.begin(19200);
   Serial4.begin(19200);
@@ -51,7 +54,7 @@ void setup()
   Serial7.begin(19200);
   delay(1);
   
-  RoveComm.begin(ROVE_FIRST_OCTET, ROVE_SECOND_OCTET, ROVE_THIRD_OCTET, DRIVEBOARD_FOURTH_OCTET); 
+  roveComm_Begin(ROVE_FIRST_OCTET, ROVE_SECOND_OCTET, ROVE_THIRD_OCTET, DRIVEBOARD_FOURTH_OCTET); 
   delay(1);
 
   pinMode(LEDSTRIP_SERVO4_PIN, OUTPUT);
@@ -68,7 +71,7 @@ void setup()
 
   NeoPixel.begin();
   
-  Watchdog.begin(roveEstopDriveMotors, 1500); 
+  Watchdog.begin(roveEstopDriveMotors, 150); 
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -78,9 +81,18 @@ void loop()
   uint16_t data_id   = 0;
   size_t   data_size = 0;
   uint8_t  data_value[4];
+  roveComm_GetMsg(&data_id, &data_size, &data_value);
+
   
-  RoveComm.read(&data_id, &data_size, &data_value);
-    
+  Serial.println("");
+  Serial.print("ID: ");
+  Serial.println(data_id);
+  Serial.print("Value: ");
+  Serial.println(data_value[0]);
+
+  digitalWrite(HEADLIGHT_PIN, LOW);
+  Serial.println("Headlights LOW");
+  
   switch (data_id) 
   {     
     case DRIVE_LEFT_RIGHT:
@@ -99,9 +111,9 @@ void loop()
     case DROP_BAY_OPEN:
     {
       uint8_t drop_bay = data_value[0];
-      
+      Serial.println("Got to Open Dropbay");
       switch (drop_bay)
-      {
+      {   
         case DROP_BAY_1:
         
           DropBay1.write(255);        
@@ -115,14 +127,15 @@ void loop()
           break; 
           
         default:
-        break; 
+          break; 
       }
+      break;
     }
     
     case DROP_BAY_CLOSE:
     {
       uint8_t drop_bay = data_value[0];
-      
+      Serial.println("Got to Close Dropbay");
       switch (drop_bay)  
       {
         case DROP_BAY_1: 
@@ -140,6 +153,7 @@ void loop()
         default:
         break; 
       }
+      break;
     }
     
     case HEADLIGHTS:
@@ -161,11 +175,23 @@ void loop()
       Watchdog.clear();
       break;
     }
-        
     default:
       break;       
   }
-    
+
+  if(notification_on)
+  {
+    headlight_loop_count ++;
+    if((headlight_loop_count % HEADLIGHT_ON_OFF_FOR_LOOPS) < HEADLIGHT_ON_OFF_FOR_LOOPS/2)
+    {
+      digitalWrite(HEADLIGHT_PIN, 1);
+    }
+    if((headlight_loop_count % HEADLIGHT_ON_OFF_FOR_LOOPS) >= HEADLIGHT_ON_OFF_FOR_LOOPS/2)
+    {
+      digitalWrite(HEADLIGHT_PIN, 0);
+    }
+  }
+  
   Serial2.write(left_drive_speed );
   Serial3.write(left_drive_speed );
   Serial4.write(left_drive_speed );  
