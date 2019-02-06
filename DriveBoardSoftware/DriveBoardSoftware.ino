d241be0126244cd27f2311140c88f9d0a03d4131
bdahlman262 <33304216+bdahlman262@users.noreply.github.com>
2019-02-06 12:28:50 -0600

    Made changes in response to the review.txt file

--- DriveBoardSoftware/DriveBoardSoftware.ino
+++ DriveBoardSoftware/DriveBoardSoftware.ino
@@ -39,13 +39,13 @@ const int switches[6] = {M1_SW, M2_SW, M3_SW, M4_SW, M5_SW, M6_SW};
 
 //////////////////////////////////////////////////
 // We send serial speed bytes to motor controllers 
-const byte LED_MAX_INTENSITY   = 255;
-const byte LED_MIN_INTENSITY   = 0;
-const byte DRIVE_MAX_FORWARD     = 64;
-const byte DRIVE_ZERO            = 0;
-//const byte DRIVE_ZERO          = 127;
-const byte RED_MAX_FORWARD       = 1000;
-const byte RED_ZERO              = 0;
+#define LED_MAX_INTENSITY          255
+#define LED_MIN_INTENSITY          0
+#define DRIVE_MAX_FORWARD          64
+#define DRIVE_ZERO                 0
+//#define DRIVE_ZERO               127
+#define RED_MAX_FORWARD            1000
+#define RED_ZERO                   0
 
 int8_t left_drive_speed        = DRIVE_ZERO;
 int8_t right_drive_speed       = DRIVE_ZERO;
@@ -54,8 +54,10 @@ int8_t right_drive_speed       = DRIVE_ZERO;
 
 int16_t motorSpeeds[6] = {0,0,0,0,0,0};
 
-int8_t left_speed_ind;
-int8_t right_speed_ind;
+int8_t direction;
+
+int8_t LEFT_SPEED_IND;
+int8_t RIGHT_SPEED_IND;
 //int8_t front_motors;  //serial 3
 //int8_t middle_motors; //serial 4
 //int8_t back_motors;   //serial 6
@@ -68,8 +70,8 @@ void setup()
   Serial3.begin(9600); //motors 1 & 4   front
   Serial4.begin(9600); //motors 2 & 5   middle
   Serial6.begin(9600); //motors 3 & 6   back
-  pinMode(LSPEED, OUTPUT);
-  pinMode(RSPEED, OUTPUT);
+  pinMode(LSPEED_IND, OUTPUT);
+  pinMode(RSPEED_IND, OUTPUT);
   RoveComm.begin(RC_DRIVEBOARD_FOURTHOCTET);
   delay(10);
 //Watchdog.begin(roveEstopDriveMotors, 150); 
@@ -93,14 +95,14 @@ void loop()
       case RC_DRIVEBOARD_DRIVELEFTRIGHT_DATAID:
       {
         //int speed[] = {LSPEED, RSPEED}; //speed values range from -1k to 1k
-        speed[0] = packet.data[0];
-        speed[1] = packet.data[1];
-        motorSpeeds[0] = speed[0];//data0
-        motorSpeeds[1] = speed[0];//data0
-        motorSpeeds[2] = speed[0];//data0
-        motorSpeeds[3] = speed[1];//data1
-        motorSpeeds[4] = speed[1];//data1
-        motorSpeeds[5] = speed[1];//data1
+        //speed[0] = packet.data[0];
+        //speed[1] = packet.data[1];
+        motorSpeeds[0] = packet.data[0];//data0
+        motorSpeeds[1] = packet.data[0];//data0
+        motorSpeeds[2] = packet.data[0];//data0
+        motorSpeeds[3] = packet.data[1];//data1
+        motorSpeeds[4] = packet.data[1];//data1
+        motorSpeeds[5] = packet.data[1];//data1
         break;
       }
       case RC_DRIVEBOARD_SPEEDRAMPVALUEs_DATAID:
@@ -112,32 +114,34 @@ void loop()
     }
   }
 ////////////////Motor control///////////////////
-  int direction = (digitalRead(DIRECTION_SW) == HIGH)? 1:-1;
-  setDriveSpeed(direction, motorSpeeds, switches);
-  sendDriveSpeed(motorSpeeds);
+  
+  motorSpeedOverride(motorSpeeds, switches);
+  setMotorControllerSpeeds(motorSpeeds);
 ////////////////////LEDs////////////////////////
-  analogWrite(left_speed_ind, map(abs(motorSpeeds[0]), RED_MAX_REVERSE, RED_MAX_FORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD)); //remaps motor speed values from 0-1000 to 0-255
-  analogWrite(right_speed_ind, map(abs(motorSpeeds[3]), RED_MAX_REVERSE, RED_MAX_FORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD));//then sets the analog value to the corresponding value
+  analogWrite(LEFT_SPEED_IND, map(abs(motorSpeeds[0]), RED_ZERO, RED_MAX_FORWARD, LED_MIN_INTENSITY, LED_MAX_INTENSITY)); //remaps motor speed values from 0-1000 to 0-255
+  analogWrite(RIGHT_SPEED_IND, map(abs(motorSpeeds[3]), RED_ZERO, RED_MAX_FORWARD, LED_MIN_INTENSITY, LED_MAX_INTENSITY));//then sets the analog value to the corresponding value
 } 
 
 ///////////////FUNCTIONS//////////////////
-void setDriveSpeed(int direction, int16_t motorSpeeds[],const int switches[])
+void motorSpeedOverride(int16_t motorSpeeds[],const int switches[])
 {
- for(int i = 0; i<6; i++)
+  
+  direction = (digitalRead(DIRECTION_SW) == HIGH)? 1:-1;
+  for(int i = 0; i<6; i++)
   {
     motorSpeeds[i] = (digitalRead(switches[i]) == HIGH)? (SWITCHMOTORSPEED*direction): motorSpeeds[i];
   }
   return;
 }
 
-void sendDriveSpeed(int16_t motorSpeeds[])//sends drive speed
+void setMotorControllerSpeeds(int16_t motorSpeeds[])//sends drive speed
 {
   for(int i = 0; i < 6 ; i++)
   {
-    //byte temp_bin_val = B00000000;
-    byte speed = B00000000;
-    byte direction = B00000000;
-    byte motor = B00000000;
+    //byte temp_bin_val         = B00000000;
+    byte speed                  = B00000000;
+    byte direction              = B00000000;
+    byte motor                  = B00000000;
     int8_t motor_speeds_sent[6] = {0,0,0,0,0,0};
     int8_t curr_motor = i;
     
@@ -145,37 +149,37 @@ void sendDriveSpeed(int16_t motorSpeeds[])//sends drive speed
     
     if(curr_motor == 0)//motor 1 (front left)
     {  
-      motor_speeds_sent[0] = map(abs(motorSpeeds[curr_motor]), RED_MAX_REVERSE, RED_MAX_FORWARD, SENT_MAX_REVERSE, SENT_MAX_FORWARD);//remaps motorspeeds to 0-64 for transmitting via serial
+      motor_speeds_sent[0] = map(abs(motorSpeeds[curr_motor]), RED_ZERO, RED_MAX_FORWARD, DRIVE_ZERO, DRIVE_MAX_FORWARD);//remaps motorspeeds to 0-64 for transmitting via serial
       motor = B00000000;
       Serial3.write(speed | direction | motor);
     }
     if(curr_motor == 1)//motor 2 (middle left)
     { 
-      motor_speeds_sent[1] = map(abs(motorSpeeds[curr_motor]), RED_MAX_REVERSE, RED_MAX_FORWARD, SENT_MAX_REVERSE, SENT_MAX_FORWARD);
+      motor_speeds_sent[1] = map(abs(motorSpeeds[curr_motor]), RED_ZERO, RED_MAX_FORWARD, DRIVE_ZERO, DRIVE_MAX_FORWARD);
       motor = B00000000;
       Serial4.write(speed | direction | motor);
     }
     if(curr_motor == 2)//motor 3 (back left)
     {  
-      motor_speeds_sent[2] = map(abs(motorSpeeds[curr_motor]), RED_MAX_REVERSE, RED_MAX_FORWARD, SENT_MAX_REVERSE, SENT_MAX_FORWARD);  
+      motor_speeds_sent[2] = map(abs(motorSpeeds[curr_motor]), RED_ZERO, RED_MAX_FORWARD, DRIVE_ZERO, DRIVE_MAX_FORWARD);  
       motor = B00000000;
       Serial6.write(speed | direction | motor);
     }
     if(curr_motor == 3)//motor 4 (front right)
     {    
-      motor_speeds_sent[3] = map(abs(motorSpeeds[curr_motor]), RED_MAX_REVERSE, RED_MAX_FORWARD, SENT_MAX_REVERSE, SENT_MAX_FORWARD); 
+      motor_speeds_sent[3] = map(abs(motorSpeeds[curr_motor]), RED_ZERO, RED_MAX_FORWARD, DRIVE_ZERO, DRIVE_MAX_FORWARD); 
       motor = B10000000;
       Serial3.write(speed | direction | motor);
     }
     if(curr_motor == 4)//motor 5 (middle right)
     {
-      motor_speeds_sent[4] = map(abs(motorSpeeds[curr_motor]), RED_MAX_REVERSE, RED_MAX_FORWARD, SENT_MAX_REVERSE, SENT_MAX_FORWARD);
+      motor_speeds_sent[4] = map(abs(motorSpeeds[curr_motor]), RED_ZERO, RED_MAX_FORWARD, DRIVE_ZERO, DRIVE_MAX_FORWARD);
       motor = B10000000;
       Serial4.write(speed | direction | motor);
     }
     if(curr_motor == 5)//motor 6 (back right)
     {
-      motor_speeds_sent[5] = map(abs(motorSpeeds[curr_motor]), RED_MAX_REVERSE, RED_MAX_FORWARD, SENT_MAX_REVERSE, SENT_MAX_FORWARD);
+      motor_speeds_sent[5] = map(abs(motorSpeeds[curr_motor]), RED_ZERO, RED_MAX_FORWARD, DRIVE_ZERO, DRIVE_MAX_FORWARD);
       motor = B10000000;
       Serial6.write(speed | direction | motor);
     }  
@@ -217,8 +221,8 @@ void sendDriveSpeed(int16_t motorSpeeds[])//sends drive speed
       int16_t right_speed_temp =  (int16_t)  speed;        // 2 low  bytes contain RED's right speed as int16
     
      // left_speed_temp   = -left_speed_temp; // Motors were wired backwards     
-      left_drive_speed  = map(left_speed_temp,  RED_MAX_REVERSE, RED_MAX_FORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD); 
-      right_drive_speed = map(right_speed_temp, RED_MAX_REVERSE, RED_MAX_FORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD);     
+      left_drive_speed  = map(left_speed_temp,  RED_ZERO, RED_MAX_FORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD); 
+      right_drive_speed = map(right_speed_temp, , RED_MAX_FORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD);     
       Watchdog.clear();
       break;  
     }
