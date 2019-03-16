6bf4b0a45a0a48cf24872bab2af1912c51da032c
Andrew Van Horn <andrewvh7@gmail.com>
2019-03-16 12:27:56 -0500

    Created pinout header

---
+++ DriveBoardSoftware/DriveBoardSoftware.h
@@ -0,0 +1,20 @@
+#ifndef _DriveBoardSoftware
+#define _DriveBoardSoftware
+
+#define DIRECTION_SWITCH_PIN    PD_2
+#define LF_BUTTON_PIN           PK_2
+#define LM_BUTTON_PIN           PK_3
+#define LR_BUTTON_PIN           PN_4
+#define RF_BUTTON_PIN           PN_5
+#define RM_BUTTON_PIN           PP_4
+#define RR_BUTTON_PIN           PQ_0
+
+#define FRONTDRIVE_SERIAL       Serial4
+#define MIDDRIVE_SERIAL         Serial6
+#define REARDRIVE_SERIAL        Serial3
+
+#define WATCHDOG_IND_PIN        PB_5
+#define LSPEED_IND_PIN          PD_5
+#define RSPEED_IND_PIN          PD_4
+
+#endif
