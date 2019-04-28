5a8bb96f21d34535db426f48e011d11f6a54847e
Andrew Van Horn <andrewvh7@gmail.com>
2019-04-27 22:41:24 -0500

    added button rovecomm speed

--- DriveBoardSoftware/DriveBoardSoftware.ino
+++ DriveBoardSoftware/DriveBoardSoftware.ino
@@ -18,6 +18,14 @@ const byte DRIVE_ZERO        = 127;
 byte left_drive_speed        = DRIVE_ZERO;
 byte right_drive_speed       = DRIVE_ZERO;
 
+struct motor
+{
+  int16_t roveCommSpeed;
+  byte serialSpeed;
+}
+
+motor Motor[6];
+
 RoveCommEthernetUdp RoveComm;
 RoveWatchdog        Watchdog;
 
@@ -38,12 +46,7 @@ void setup()
   
   RoveComm.begin(RC_DRIVEBOARD_FOURTHOCTET); 
   delay(1);
-  
-  //
-  
-  
-  
-  
+
   Watchdog.begin(roveEstopDriveMotors, 150); 
   delay(1);
 }
@@ -78,24 +81,40 @@ void loop()
         int16_t right_speed_temp =  packet.data[1];        // 2 low  bytes contain RED's right speed as int16
   
         Serial.print("RoveComm Speed:"); Serial.print(left_speed_temp); Serial.print(","); Serial.println(right_speed_temp);
+
+        Motor[0].roveCommSpeed = left_speed_temp;
+        Motor[1].roveCommSpeed = left_speed_temp;
+        Motor[2].roveCommSpeed = left_speed_temp;
+        Motor[3].roveCommSpeed = right_speed_temp;
+        Motor[4].roveCommSpeed = right_speed_temp;
+        Motor[5].roveCommSpeed = right_speed_temp;
         
-        //right_speed_temp = right_speed_temp * (-1);
-         
-        left_drive_speed  = map(left_speed_temp,  RC_DRIVEBOARD_DRIVELEFTRIGHT_DRIVEMAXREVERSE, RC_DRIVEBOARD_DRIVELEFTRIGHT_DRIVEMAXFORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD); 
-        right_drive_speed = map(right_speed_temp, RC_DRIVEBOARD_DRIVELEFTRIGHT_DRIVEMAXREVERSE, RC_DRIVEBOARD_DRIVELEFTRIGHT_DRIVEMAXFORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD);     
-        Serial.print("Drive Speed:"); Serial.print(left_drive_speed); Serial.print(","); Serial.println(right_drive_speed);
+        Watchdog.clear();
+        break;  
+      }
+      case RC_DRIVEBOARD_DRIVEMOTORS_DATAID:
+      {         
+        for(int i = 0; i<6; i++)
+        {
+          Motor[0].roveCommSpeed = packet.data[i];
+        }
         Watchdog.clear();
         break;  
       }
     }
   }
+
+  for(int i = 0; i<6; i++)
+  {
+    Motor[i].serialSpeed = map(Motor[i].roveCommSpeed, RC_DRIVEBOARD_DRIVELEFTRIGHT_DRIVEMAXREVERSE, RC_DRIVEBOARD_DRIVELEFTRIGHT_DRIVEMAXFORWARD, DRIVE_MAX_REVERSE, DRIVE_MAX_FORWARD);
+  }
   
-  Serial2.write(left_drive_speed );
-  Serial3.write(left_drive_speed );
-  Serial4.write(left_drive_speed );  
-  Serial5.write(right_drive_speed);
-  Serial6.write(right_drive_speed);
-  Serial7.write(right_drive_speed);
+  Serial2.write(Motor[0].serialSpeed);  //LF
+  Serial3.write(Motor[1].serialSpeed ); //LM
+  Serial4.write(Motor[2].serialSpeed ); //LR
+  Serial5.write(Motor[3].serialSpeed ); //RF
+  Serial6.write(Motor[4].serialSpeed);  //RM
+  Serial7.write(Motor[5].serialSpeed);  //RR
   delay(1);
 }
 
