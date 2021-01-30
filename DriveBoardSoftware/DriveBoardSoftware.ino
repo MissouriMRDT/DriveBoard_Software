bafe52cea8265303b0acf5e5e934a768fcf8b4f7
labbloe <jla7ym@mst.edu>
2021-01-30 14:06:30 -0600

    Moved CAN test, Updated ODrive mode control
    CAN test was moved to its own folder for later testing and to clean up main. ODrive mode set to Closed Loop in setup, removed all unecessary code and updated some comments.

--- DriveBoardSoftware/DriveBoardSoftware.ino
+++ DriveBoardSoftware/DriveBoardSoftware.ino
@@ -1,4 +1,3 @@
-
 //DriveBoard Software Rev 1 2021
 
 #include "DriveBoardSoftware.h"
@@ -9,8 +8,7 @@
 /////////////////////////////////////////////////////////////////////////
 EthernetServer TCPServer(RC_ROVECOMM_ETHERNET_DRIVE_LIGHTING_BOARD_PORT);
 
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 void setup() {
   // initialize all serial ports: 
   Serial.begin(19200);
@@ -23,34 +21,11 @@ void setup() {
   LeftOdrive.begin(LEFT_ODRIVE_SERIAL);         //OD1
   RightOdrive.begin(RIGHT_ODRIVE_SERIAL);       //OD2
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////
-  // CAN ODrive Communication
-  //////////////////////////////////////////////////////////////////////////////////////////////
-  /*tCANBitClkParms CANBitClk;
-  tCANMsgObject sMsgObjectRx;
-  tCANMsgObject sMsgObjectTx;
-  unsigned int buffer;                                    //4 byte message data
-  unsigned char *bufferPtr = (unsigned char *)&buffer;    //access individual bytes using pointer
-
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_CAN0);             //Enable the CAN0 module
-  CANInit(CAN0_BASE);                                     //initialize CAN0 module
-  CANBitRateSet(CAN0_BASE, SysCtlClockGet(), 1000000u);   //Configure controller for 1Mbit operation (MCP2551 transceiver supports up to 1Mb/s)
-  CANEnable(CAN0_BASE);                                   //Take CAN0 out of INIT state
-
-  //TEST MSG
-  bufferPtr[0] = 128;
-  bufferPtr[1] = 127;
-  bufferPtr[2] = 126;
-  bufferPtr[3] = 125;
-
-  //CAN Transmit Configuration
-  sMsgObjectTx.ui32MsgID = 0x400u;
-  sMsgObjectTx.ui32Flags = 0u;
-  sMsgObjectTx.ui32MsgLen = sizeof(bufferPtr);  //4 byte max message
-  sMsgObjectTx.pui8MsgData = bufferPtr;
-
-  //TEST TRANSMISSION!
-  CANMessageSet(CAN0_BASE, 1, &sMsgObjectTx, MSG_OBJ_TYPE_TX);  //send as msg object 1   */
+  // Set ODrive Closed Loop Control Mode
+  LeftOdrive.left.writeState(  AXIS_STATE_CLOSED_LOOP_CONTROL);
+  LeftOdrive.right.writeState( AXIS_STATE_CLOSED_LOOP_CONTROL);
+  RightOdrive.left.writeState( AXIS_STATE_CLOSED_LOOP_CONTROL);
+  RightOdrive.right.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
 
   //////////////////////////////////////////////////////
   RoveComm.begin(RC_DRIVEBOARD_FOURTHOCTET, &TCPServer);
@@ -74,14 +49,14 @@ void setup() {
   WireBreaks.start();
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
 void loop() {
     packet = RoveComm.read();
   
     switch(packet.data_id)
     {
       //////////////////////////////////////////////////////////
-      //TANKDRIVE
+      //Read in Left/Right Wheel Speeds
       //////////////////////////////////////////////////////////
       case RC_DRIVEBOARD_DRIVELEFTRIGHT_DATAID:
         int16_t *leftrightspeeds;
@@ -145,7 +120,6 @@ void loop() {
       case RC_DRIVEBOARD_WATCHDOG_DATAID:
         uint8_t *watchdogState;
         watchdogState = (uint8_t*)packet.data;  //1- Turn Off watchdog, 0- turn on
-        //if()
     }
 
     //check for button presses and override speeds if so
@@ -198,19 +172,11 @@ void Estop()
 ////////////////////////////////////////////////////////
 void swerveDriveInit(uint8_t *wheelAngle)
 {
-  int maxDegreeDifference = DEGREE_ALLOWABLE_INIT_DIFFERENCE + 1;
-  
   //Ensure all motors are at DRIVE_ZERO before initiating wheel turn
   FL_SERIAL.write(DRIVE_ZERO); //FL
   FR_SERIAL.write(DRIVE_ZERO); //FR
   RL_SERIAL.write(DRIVE_ZERO); //RL
   RR_SERIAL.write(DRIVE_ZERO); //RR
-  
-  //change ODrive state
-  LeftOdrive.left.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-  LeftOdrive.right.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-  RightOdrive.left.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-  RightOdrive.right.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
 
   //Send move command and angle to ODrives. 
   //Function returns once wheels are at correct angle (i.e. blocks packets)
@@ -222,12 +188,6 @@ void swerveDriveInit(uint8_t *wheelAngle)
   RightOdrive.left.updateWatchdog();
   RightOdrive.right.updateWatchdog();
 
-  //Return ODrive state to Idle
-  LeftOdrive.left.writeState(AXIS_STATE_IDLE);
-  LeftOdrive.right.writeState(AXIS_STATE_IDLE);
-  RightOdrive.left.writeState(AXIS_STATE_IDLE);
-  RightOdrive.right.writeState(AXIS_STATE_IDLE);
-
   //Return to loop and run at given motor speeds
 }
 
@@ -236,8 +196,6 @@ void swerveDriveInit(uint8_t *wheelAngle)
 ////////////////////////////////////////////////////////
 void pointTurn(uint8_t *wheelAngle)
 {
-  int maxDegreeDifference = DEGREE_ALLOWABLE_INIT_DIFFERENCE + 1;
-  
   //Prepare each wheel for point turn
   wheelAngle[0] = 45;     //FL
   wheelAngle[1] = 135;    //FR
@@ -251,12 +209,6 @@ void pointTurn(uint8_t *wheelAngle)
   RL_SERIAL.write(DRIVE_ZERO); //RL
   RR_SERIAL.write(DRIVE_ZERO); //RR
 
-  //Update ODrive Watchdogs
-  LeftOdrive.left.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-  LeftOdrive.right.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-  RightOdrive.left.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-  RightOdrive.right.writeState(AXIS_STATE_CLOSED_LOOP_CONTROL);
-
   //Send move command and angle to ODrives
   moveWheelsToAngle(wheelAngle);
 
@@ -265,38 +217,6 @@ void pointTurn(uint8_t *wheelAngle)
   LeftOdrive.right.updateWatchdog();
   RightOdrive.left.updateWatchdog();
   RightOdrive.right.updateWatchdog();
-
-  //wait until all wheels are within allowable difference range before driving
-  while(maxDegreeDifference > DEGREE_ALLOWABLE_INIT_DIFFERENCE)
-  {
-    for(int i=0; i<4; i++)     
-      maxDegreeDifference = abs(encoders[i].readDegrees() - wheelAngle[i]);
-  }
-
-  /*
-  if(turnSpeed < 0)                     //counter-clockwise
-  {
-    FL_SERIAL.write(motorSpeeds[0]); //FL
-    FR_SERIAL.write(-motorSpeeds[1]); //FR
-    RL_SERIAL.write(-motorSpeeds[2]); //RL
-    RR_SERIAL.write(motorSpeeds[3]); //RR
-  }
-
-  if(turnSpeed > 0)                     //clockwise
-  {
-    FL_SERIAL.write(-motorSpeeds[0]); //FL
-    FR_SERIAL.write(motorSpeeds[1]); //FR
-    RL_SERIAL.write(motorSpeeds[2]); //RL
-    RR_SERIAL.write(-motorSpeeds[3]); //RR
-  }
-   */
-
-  //Return ODrive state to Idle
-  LeftOdrive.left.writeState(AXIS_STATE_IDLE);
-  LeftOdrive.right.writeState(AXIS_STATE_IDLE);
-  RightOdrive.left.writeState(AXIS_STATE_IDLE);
-  RightOdrive.right.writeState(AXIS_STATE_IDLE);
-
 }
 
 //NOTE: Rover should be stopped before initiating a wheel turn.
