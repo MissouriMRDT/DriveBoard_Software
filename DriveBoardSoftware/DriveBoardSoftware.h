6cd93305df42c1cb5e1a9aa4f8b36c03275cf00c
jtschmidtt <90575971+jtschmidtt@users.noreply.github.com>
2022-01-26 18:01:33 -0600

    Header Fix

--- DriveBoardSoftware/DriveBoardSoftware.h
+++ DriveBoardSoftware/DriveBoardSoftware.h
@@ -4,28 +4,34 @@
 #include "RoveComm.h"
 #include <VescUart.h>
 #include "RoveWatchdog.h"
-#include <math.h>
 
 //Motor Overide Buttons
-#define LF_MOTOR                PL_4
-#define LR_MOTOR                PL_0
-#define RF_MOTOR                PL_5
-#define RR_MOTOR                PL_1
+#define FL_MOTOR                PG_1
+#define FR_MOTOR                PK_4
+#define ML_MOTOR                PK_5
+#define MR_MOTOR                PM_0
+#define BL_MOTOR                PM_1
+#define BR_MOTOR                PM_2
 #define DIR_SWITCH              PP_2
 #define BUTTON_OVERIDE_SPEED    5000
 
 //Motor Speed Controls
-#define LF_SERIAL               Serial2
-#define LR_SERIAL               Serial4
-#define RF_SERIAL               Serial3
-#define RR_SERIAL               Serial6
+#define FL_SERIAL               Serial7
+#define FR_SERIAL               Serial6
+#define ML_SERIAL               Serial5
+#define MR_SERIAL               Serial4
+#define BL_SERIAL               Serial3
+#define BR_SERIAL               Serial2
 #define DRIVE_MIN_RPM           0
-#define DRIVE_MAX_RPM           35000
+#define DRIVE_MAX_RPM           14000
 
-VescUart LF_UART;
-VescUart LR_UART;
-VescUart RF_UART;
-VescUart RR_UART;
+//Vesc Serial
+VescUart FL_UART;
+VescUart FR_UART;
+VescUart ML_UART;
+VescUart MR_UART;
+VescUart BL_UART;
+VescUart BR_UART;
 
 //Rovecomm
 RoveCommEthernet RoveComm;
@@ -33,25 +39,13 @@ rovecomm_packet packet;
 RoveWatchdog Watchdog;
 uint32_t last_update_time;
 
-//All wheels are in order of LF,LR,RF,RR
-uint8_t motorButtons[4] = { LF_MOTOR, LR_MOTOR, RF_MOTOR, RR_MOTOR };
-int16_t motorSpeeds[4] = {}; //LF,LR,RF,RR
-float wheelAngle[4] = {}; //LF,LR,RF,RR
-int32_t turnSpeeds[4] = {}; //LF,LR,RF,RR
-float motorCurrent[4] = {};
-uint16_t absoluteAngles[4] =  {};
-float incrementAngleHome[4] = {};
-float absoluteOffset[4] = { -150000, 83000, 160000, -57000};
-float incrementalCWOffset[4] = {};
-float incrementalCCWOffset[4] = {};
-float wheelDirectionFactor[4] = { 1, -1, -1, 1 };
+//All wheels are in order of FL, FR, ML, MR, BL, BR
+uint8_t motorButtons[6] = {FL_MOTOR, FR_MOTOR, ML_MOTOR, MR_MOTOR, BL_MOTOR, BR_MOTOR};
+int16_t motorSpeeds[6] = {}; //FL, FR, ML, MR, BL, BR
+float motorCurrent[6] = {};
 
+//Estop Decleration
 void EStop();
-void swerveDriveInit();
-void pointTurn();
-void moveWheelsToAngle(float *goalAngle);
-void printUARTdata(VescUart UART);
-void incrementAngle(float wheelAngle);
-float signMin(float angle, float angle2);
+
 
 #endif
